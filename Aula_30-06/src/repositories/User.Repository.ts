cb29ae41a2740aea4c6c7314474a0029811d74d1
JIAN LUCA PALINI <03210018006@senacrs.edu.br>
2026-07-01 17:16:54 -0300

    feat: continuação aula

---
+++ Aula_30-06/src/repositories/User.Repository.ts
@@ -0,0 +1,31 @@
+import { create } from "node:domain";
+import { AppDataSource } from "../config/data-source";
+import { User } from "../models/User";
+
+// Um repository (repositório) é um objeto do TypeORM que contém todas as funções que precisamos
+// para trabalhar com o banco, ligado a uma entidade específica (nesse caso, User)
+const repo = AppDataSource.getRepository(User)
+
+export const UserRepository = {
+    // Aqui vamos criar os métodos que fazem o CRUD de usuário
+
+    // Busca todos os usuários
+    async findAll() {
+        // O método find() vem do TypeORM. Ele procura algo em uma tabela
+        // ele aceita como parâmetro um objeto com opções para esta busca
+        // nesse nosso caso, estamos buscando também os posts relacionados com este usuários, ou seja, quando buscarmos o usuário 'Joãozinho', o servidor também vai retornar no JSON todos os posts dele, incluindo a vez em que ele xingou sua tia
+        return repo.find({relations: ['posts']})
+    },
+
+    async findById(id:number){
+        return repo.findOne({where:{id}, relations:['posts']})
+    },
+
+    async create(data:User){
+        // cria o usuário
+        const user = repo.create(data)
+        // salva ele no banco
+        return repo.save(user)
+    }
+
+}
